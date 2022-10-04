7004e3e9290d03d6aa3981757e0f8a73cba080fc
ondrui <ondrui@yandex.ru>
2022-10-05 00:03:29 +0300

    add new lessons

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,23 @@
-"use strict";
 // Аннотация или указание типа
+
 // примитивные типы данных
 // let revenue: number = 1000;
 // let bonus: number = 500;
 // let c = 'sdd';
 // let d: boolean = true;
+
 // let res: number = revenue + bonus;
 // console.log(res);
+
 // типы в функциях
 // function getFullName(firstname: string, surname: string): string {
 //   return `${firstname} ${surname}`;
 // }
+
 // const getFullNameArrow = (firstname: string, surname: string): string  => {
 //   return `${firstname} ${surname}`;
 // };
+
 //так в TS не делают практически никогда
 // function getFullName(firstname, surname) {
 //   if (typeof firstname !== 'string') {
@@ -21,11 +25,15 @@
 //   }
 //   return `${firstname} ${surname}`;
 // }
+
 //console.log(getFullName('Andrey', 'Solo'));
+
 //Объекты
+
 // function getFullName(userEntity: {firstname: string, surname: string}): string {
 //   return `${userEntity.firstname} ${userEntity.surname}`;
 // }
+
 // const user = {
 //   firstname: 'Andrey',
 //   surname: 'Solo',
@@ -36,61 +44,84 @@
 //     devops: true
 //   }
 // };
+
 // console.log(getFullName(user));
+
 //Массивы
+
 // const skills: string[] = ['Dev', 'DevOps', 'Testing'];
+
 // for (const skill of skills) {
 //   console.log(skill.toLocaleLowerCase());
 // }
+
 // const res = skills
 //       .filter((s: string) => s !== 'DevOps')
 //       .map(s => s + '! ')
 //       .reduce((a, b) => a + b);
 // console.log(res);
+
 // Tuples Кортежи
 //const skill: [number, string] = [1, 'Dev'];
 // const id = skill[0];
 // const skillName = skill[1];
 // //const q = skill[2]; error
 // skill.push('dfddf');
+
 // const [id, skillName] = skill;
+
 // const arr: [number, string, ...boolean[]] = [1, 'ssss', true, false];
+
 //Readonly
 //const skill: readonly [number, string] = [1, 'Dev'];
+
 //skill[1] = 3;
+
 //const skills: readonly string[] = ['Dev', 'DevOps'];
 //const skills: ReadonlyArray<string> = ['Dev', 'DevOps'];
 //skills[1] = 'dsd';
 //skills.push('sdds'); error
+
 //Enums
+
 // enum StatusCode {
 //   SUCCESS = 1,
 //   IN_PROCESS = 2,
 //   FAILED = 3
 // }
+
 // const res = {
 //   message: 'Платеж успешен',
 //   statusCode: StatusCode.SUCCESS
 // };
+
 //статусы
 // 1 - успех
 // 2 - в процессе
 // 3 - отклонен
 // if (res.statusCode === StatusCode.SUCCESS) {
+
 // }
+
 // const enum Roles {
 //   ADMIN = 1,
 //   USER = 2
 // };
+
 // const res2 = Roles.ADMIN;
+
 // Union
+
 // function logId(id: string | number | boolean ) {
 //   console.log(id);
 // }
+
 // logId(1);
 // logId('sfgef');
 // logId(true);
+
 //Narrowing сужение типов
+
 // function logId(id: string | number | boolean) {
 //   if (typeof id === 'string') {
 //     console.log(id);
@@ -100,6 +131,7 @@
 //     console.log(id);
 //   }
 // }
+
 // function logError(err: string | string[]) {
 //   if (Array.isArray(err)) {
 //     console.log(err);
@@ -114,104 +146,138 @@
 //     console.log(obj.b);
 //   }
 // }
+
 // function logMultipleIds(a: string | number, b: string | boolean) {
 //   if (a === b) {
 //     a.toLowerCase();
 //   } else {
 //     console.
 // }
+
 //Literal Types
+
 // function fetchWithAuth(url: string, method: 'post' | 'get'): 1 | -1 {
 //   return 1;
 // }
+
 //fetchWithAuth('sfd', 'get');
 //fetchWithAuth('sfd', 'l'); error
+
 // let method = 'post';
+
 //fetchWithAuth('lkll', method as 'post'); error
 // fetchWithAuth('lkll', method as 'post');
+
 //Type Aliases
 // type httpMethod = 'post' | 'get';
+
 // function fetchWithAuth(url: string, method: httpMethod): 1 | -1 {
 //   return 1;
 // }
+
 // type User = {
 //   name: string;
 //   age: number;
 //   skills: string[];
 // };
+
 // let user: User = {
 //   name: 'ssdg',
 //   age: 33,
 //   skills: ['1', '2'],
 // };
+
 //Intersection Types
+
 // type Role = {
 //   id: number;
 // };
+
 // type UserWithRole = User & Role;
+
 // let user: UserWithRole = {
 //   name: 'ssdg',
 //   age: 33,
 //   skills: ['1', '2'],
 //   id: 222
 // };
+
 //Interfaces
+
 // interface User {
 //   name: string;
 //   age: number;
 //   skills: string[];
+
 //   log: (id: number) => string;
 // }
+
 // interface Role {
 //   roleId: number;
 // }
+
 // interface UserWithRole extends User, Role {
 //   createdAt: Date;
 // }
+
 // type User2 = {
 //   name: string;
 //   age: number;
 //   skills: string[];
+
 //   log: (id: number) => string;
 // };
+
 // let user: UserWithRole = {
 //   name: 'ssdg',
 //   age: 33,
 //   skills: ['1', '2'],
 //   roleId: 321,
 //   createdAt: new Date(),
+
 //   log(id) {
 //     return '';
 //   },
 // };
+
 // interface UserDic {
 //   [index: number]: User;
 // }
+
 // type UserDic2 = {
 //   [index: number]: User;
 // };
+
 // type ud = Record<number, User>;
+
 //Types или Interfaces?
+
 //merge interface
+
 // interface User {
 //   name: string
 // }
+
 // interface User {
 //   age: number,
 //   job: string
 //   city: string
 // }
+
 // const user: User = {
 //   name: 'Ann',
 //   age: 22,
 //   job: 'DevOps',
 //   city: 'Omsk'
 // }
+
 // ------- Classes in TS -------------
 //----------------------
+
 // class User {
 //   name: string;
 //   age: number;
+
 // Overloads
 //   constructor();
 //   constructor(name: string);
@@ -228,32 +294,40 @@
 //     }
 //   }
 // }
+
 // const user = new User('Вася');
 // const user2 = new User();
 // const user3 = new User(33);
 // const user4 = new User('Вася', 33);
+
 // class Admin {
 //   role: number;
 // }
+
 // const admin = new Admin();
 // admin.role  = 1;
+
 // Methods
 // enum  PaymentStatus {
 //   Holded,
 //   Processed,
 //   Reversed
 // }
+
 // class Payment {
 //   id: number;
 //   status: PaymentStatus = PaymentStatus.Holded;
 //   createdAt: Date = new Date();
 //   updateAt: Date;
+
 //   constructor(id: number) {
 //     this.id = id;
 //   }
+
 //   getPaymentLifeTime(): number {
 //     return new Date().getTime() - this.createdAt.getTime();
 //   }
+
 //   unholdPayment(): void {
 //     if (this.status == PaymentStatus.Processed) {
 //       throw new Error('Платеж не может быть возвращен!')
@@ -262,13 +336,16 @@
 //     this.updateAt = new Date();
 //   }
 // }
+
 // const payment = new Payment(1);
 // payment.unholdPayment();
 // console.log(payment);
 // const time = payment.getPaymentLifeTime();
 // console.log(time);
+
 // class User {
 //   skills: string[];
+
 //   addSkill(skill: string): void;
 //   addSkill(skills: string[]): void;
 //   addSkill(skillOrSkills: string | string[]): void {
@@ -278,9 +355,12 @@
 //       this.skills.concat(skillOrSkills);
 //     }
 //   }
+
 // }
+
 // new User().addSkill()
 // Overloads function
+
 // function run(distance: string): string;
 // function run(distance: number): number;
 // function run(distance: number | string): number | string {
@@ -290,28 +370,35 @@
 //   return distance;
 // }
 // run(1);
+
 // -- getter & setter --
+
 // class User {
 //   _login: string;
 //   password: string;
+
 //   set login(l: string) {
 //     this._login = 'user-' + l;
 //   }
+
 //   get login() {
 //     return 'no_login';
 //   }
 // }
+
 // const user = new User();
 // //bad case
 // // user.login = 'user-';
 // user.login = 'myLogin';
 // console.log(user);
 // console.log(user.login);
+
 // -- Implements --
 // interface ILogger {
 //   log(...args: number[]): void;
 //   error(...args: number[]): void;
 // }
+
 // class Logger implements ILogger {
 //   log(...args: any[]): void {
 //     console.log(...args);
@@ -321,13 +408,16 @@
 //     console.log(...args);
 //   }
 // }
+
 // interface IPayable {
 //   pay(paymentId: number): void;
 //   price?: number;
 // }
+
 // interface IDeleteable {
 //   delete(): void;
 // }
+
 // class User implements IPayable, IDeleteable {
 //   delete(): void {
 //     throw new Error("Method not implemented.");
@@ -337,25 +427,32 @@
 //   }
 //   price?: number | undefined;
 // }
+
 // -- Extends --
 // type PaymentStatus = 'new' | 'paid';
+
 // class Payment {
 //   id: number;
 //   status: PaymentStatus = 'new';
+
 //   constructor(id: number) {
 //     this.id = id;
 //   }
+
 //   pay() {
 //     this.status = 'paid';
 //   }
 // }
+
 // class PersistedPayment extends Payment {
 //   databaseId: number;
 //   paidAt: Date;
+
 //   constructor() {
 //     const id = Math.random();
 //     super(id);
 //   }
+
 //   save() {
 //     // Сохраняет в базу
 //   }
@@ -367,8 +464,11 @@
 //     }
 //   }
 // }
+
 // new PersistedPayment();
+
 // -- Typescript - Generics, Index Access Types, Keyof на простом примере --
+
 // const menu = {
 //   analytycs: {
 //     bussines: "Для бизнеса",
@@ -378,6 +478,7 @@
 //     graphical: "Графический",
 //   },
 // };
+
 // function getMenu<T, L1 extends keyof T, L2 extends keyof T[L1]>(
 //   obj: T,
 //   l1: L1,
@@ -385,18 +486,23 @@
 // ): T[L1][L2] {
 //   return obj[l1][l2];
 // }
+
 // const res = getMenu(menu, 'design', 'graphical');
+
 //   pay() {
 //     this.status = 'paid';
 //   }
 // }
+
 // class PersistedPayment extends Payment {
 //   databaseId: number;
 //   paidAt: Date;
+
 //   constructor() {
 //     const id = Math.random();
 //     super(id);
 //   }
+
 //   save() {
 //     // Сохраняет в базу
 //   }
@@ -408,189 +514,258 @@
 //     }
 //   }
 // }
+
 // new PersistedPayment();
+
 // class User {
 //   name: string = 'user';
+
 //   constructor() {
 //     console.log(this.name);
 //   }
 // }
+
 // class Admin extends User {
 //   name: string = 'admin';
+
 //   constructor() {
 //     super();
 //     console.log(this.name);
 //   }
 // }
+
 // new Admin();
+
 // new Error('');
+
 // class HttpError extends Error {
 //   code: number;
+
 //   constructor(message: string, code?: number) {
 //     super(message);
 //     this.code = code ?? 500;
+
 //   }
 // }
+
 // -- Композиция против наследования --
+
 // class User {
 //   name: string;
+
 //   constructor(name: string) {
 //     this.name = name;
 //   }
 // }
+
 // // Наследование
 // // bad case
 // class Users extends Array<User> {
 //   searchByName(name: string) {
 //     return this.filter(u => u.name === name);
 //   }
+
 //   override toString(): string {
 //     return this.map(u => u.name).join(', ');
 //   }
 // }
+
 // const users =  new Users();
 // users.push(new User('Вася'));
 // users.push(new User('Петя'));
 // console.log(users.toString());
+
 // // Композиция
 // // good!
 // class UsersList {
 //   users: User[];
+
 //   push(u: User) {
 //     this.users.push(u);
 //   }
 // }
+
 // class Payment {
 //   date: Date;
 // }
+
 // // Наследование
 // class UserWithPayment extends Payment {
 //   name: string;
 // }
+
 // // Композиция
 // class UserWithPayment2 {
 //   user: User;
 //   payment: Payment;
+
 //   constructor(user: User, payment: Payment) {
 //     this.payment = payment;
 //     this.user = user;
 //   }
 // }
+
 // -- 46. Видимость свойств --
+
 // class Vehicle {
 //   public make: string;
 //   private damages: string[];
 //   private _model: string;
 //   #price: number;
+
 //   // доступно у наследников
 //   protected run: number;
+
 //   set model(m: string) {
 //     this._model = m;
 //     this.#price = 199;
 //   }
+
 //   get model() {
 //     return this._model;
 //   }
+
 //   isPriceEqual(v: Vehicle) {
 //     this.#price === v.#price;
 //   }
+
 //   private addDamage(damage: string) {
 //     this.damages.push(damage);
 //   }
 // }
+
 // class EuroTruck extends Vehicle {
 //   setRun(km: number) {
 //     this.run = km / 0.62;
 //     //this.damages - error
 //   }
 // }
+
 // new Vehicle();
+
 // -- 48. Статические свойства --
+
 // class UserService {
 //   //static name: string = 'sdf'; Error!!! Static property 'name' conflicts with built-in property 'Function.name' of constructor function 'UserService'.
 //   static db: any;
+
 //   static getUser(id: number) {
 //     return UserService.db.findById(id);
 //   }
+
 //   create() {
 //     UserService.db;
 //   }
+
 //   static {
 //     UserService.db = 'sdf';
 //     // no async
 //   }
 // }
+
 // UserService.db;
 // UserService.getUser(3);
 // const inst = new UserService();
 // inst.create();\
+
 // -- 49. Рабата с this --
+
 // class Payment {
 //   private date: Date = new Date;
+
 //   getDate(this: Payment) {
 //     return this.date;
 //   }
+
 //   // Стрелочные функции не теряют контекст this!
 //   getDateArrow = () => {
 //     return this.date;
 //   }
 // }
+
 // const p = new Payment();
+
 // const user = {
 //   id: 1,
 //   paymentDate: p.getDate.bind(p),
 //   paymentDateArrow: p.getDateArrow,
 // };
+
 // // console.log(p.getDate());
 // // console.log(user.paymentDate());
 // // console.log(user.paymentDateArrow()); //2022-10-04T13:34:46.021Z ok
+
 // class PaymentPersistent extends Payment {
 //   save() {
 //     return super.getDateArrow();
 //   }
 // }
+
 // // console.log(new PaymentPersistent().save());// TypeError: (intermediate value).getDateArrow is not a function
+
 // class PaymentPersistent1 extends Payment {
 //   save() {
 //     return this.getDateArrow();
 //   }
 // }
+
 // console.log(new PaymentPersistent1().save()); // 2022-10-04T13:42:19.908Z
+
 // -- 50. Типизация this --
+
 // class UserBuilder {
 //   name: string;
+
 //   setName(name: string): this {
 //     this.name = name;
 //     return this;
 //   }
+
 //   isAdmin(): this is AdminBuilder {
 //     return this instanceof AdminBuilder;
 //   }
 // }
+
 // class AdminBuilder extends UserBuilder {
 //   roles: string[];
 // }
+
 // const res = new UserBuilder().setName('Вася');
 // const res2 = new AdminBuilder().setName('Вася');
+
+
 // let user: UserBuilder | AdminBuilder = new UserBuilder();
+
 // if (user.isAdmin()) {
 //   console.log(user);
 // } else {
 //   console.log(user);
 // }
+
 // -- 51. Абстрактные классы --
-class Controller {
-    handleWithLogs(req) {
-        console.log('Start');
-        this.handle(req);
-        console.log('End');
-    }
+
+abstract class Controller {
+  abstract handle(req: any): void;
+
+  handleWithLogs(req: any) {
+    console.log('Start');
+    this.handle(req);
+    console.log('End');
+  }
 }
+
 class UserController extends Controller {
-    handle(req) {
-        console.log(req);
-    }
+  handle(req: any): void {
+    console.log(req);
+  }
 }
-// new Controller() - error: Cannot create an instance of an abstract class.
+
+//new Controller() - error: Cannot create an instance of an abstract class.
 const c = new UserController();
 c.handleWithLogs('Request');
+
+// -- 56. Включение и исключение файлов --
+
+// -- 57. Вывод компиляции --
+
+/* Emit */
--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,3 @@
-"use strict";
 //------------1-------
 // let info: {
 //   officeId: number,
@@ -21,12 +20,15 @@
 // 		}
 // 	}
 // }
+
 //-------------2----
+
 // enum StatusCode {
 //   PUBLISHED = 'published',
 //   DRAFT = 'draft',
 //   DELETED = 'deleted',
 // }
+
 // async function getFaqs(req: { topicId: number; status: StatusCode }): Promise<
 //   {
 //     question: string;
@@ -43,20 +45,26 @@
 //   const data = await res.json();
 //   return data;
 // }
+
 //-------------3----
+
 // interface IPayment {
 //   sum: number;
 //   from: number;
 //   to: number;
 // }
+
 // interface IPaymentRequest extends IPayment {}
+
 // enum PaymentStatus {
 //   Success = 'success',
 //   Failed = 'failed',
 // }
+
 // interface IDataSuccess extends IPayment {
 //   databaseId: number;
 // }
+
 // interface IDataFailed {
 //   errorMessage: string;
 //   errorCode: number;
@@ -66,6 +74,7 @@
 //   status: PaymentStatus;
 //   data: IDataSuccess | IDataFailed;
 // }
+
 // //good
 // interface IResponseSuccess {
 //   status: PaymentStatus.Success;
@@ -75,14 +84,18 @@
 //   status: PaymentStatus.Failed;
 //   data: IDataFailed;
 // }
+
 // type Res = IResponseSuccess | IResponseFailed;
+
 // -- 4 --
+
 // function isSuccess(res: Res): res is IResponseSuccess {
 //   if(res.status === PaymentStatus.Success) {
 //     return true;
 //   }
 //   return false;
 // }
+
 // function geIdFromData(res: Res): number {
 //   if (isSuccess(res)) {
 //     return res.data.databaseId;
@@ -90,12 +103,14 @@
 //     throw new Error(res.data.errorMessage);
 //   }
 // }
+
 // function isSuccess(res: Res): res is IResponseSuccess {
 //   if(res.status === PaymentStatus.Success) {
 //     return true;
 //   }
 //   return false;
 // }
+
 // function geIdFromData(res: Res): number {
 //   if (isSuccess(res)) {
 //     return res.data.databaseId;
@@ -103,7 +118,9 @@
 //     throw new Error(res.data.errorMessage);
 //   }
 // }
+
 // -- 5 --
+
 /*
 Необходимо сделать корзину (Cart) на сайте,
 которая имееет список продуктов (Product), добавленных в корзину
@@ -116,6 +133,7 @@
 Product: id, название и цена
 Delivery: может быть как до дома (дата и адрес) или до пункта выдачи (дата = Сегодня и Id магазина)
 */
+
 // class Product {
 //   constructor(
 //     public id: number,
@@ -123,39 +141,49 @@ Delivery: может быть как до дома (дата и адрес) ил
 //     public price: number
 //   ) {}
 // }
+
 // class Delivery {
 //   constructor(
 //     public date: Date
 //   ) {}
 // }
+
 // class HomeDelivery extends Delivery {
 //   constructor(date: Date, public address: string) {
 //     super(date);
 //   }
 // }
+
 // class ShopDelivery extends Delivery {
 //   constructor(public shopId: number) {
 //     super(new Date());
 //   }
 // }
+
 // type DeliveryOptions = HomeDelivery | ShopDelivery;
+
 // class Cart {
 //   private products: Product[] = [];
 //   private delivery: DeliveryOptions;
+
 //   public addProduct(product: Product): void {
 //     this.products.push(product);
 //   }
+
 //   public deleteProductById(productId: number): void {
 //     this.products =  this.products.filter((p: Product) => p.id !== productId);
 //   }
+
 //   public getSum(): number {
 //     return this.products
 //       .map((p: Product) => p.price)
 //       .reduce((p1: number, p2: number) => p1 + p2);
 //   }
+
 //   public setDelivery(delivery: DeliveryOptions): void {
 //     this.delivery = delivery;
 //   }
+
 //   public checkOut() {
 //     if (this.products.length == 0) {
 //       throw new Error('Нет ни одного товара в корзине');
@@ -166,14 +194,17 @@ Delivery: может быть как до дома (дата и адрес) ил
 //     return { success: true };
 //   }
 // }
+
 // const cart = new Cart();
 // cart.addProduct(new Product(1, 'Печенье', 10));
 // cart.addProduct(new Product(2, 'Сироп', 30));
 // cart.addProduct(new Product(3, 'Молоко', 40));
 // cart.deleteProductById(1);
 // // cart.setDelivery(new HomeDelivery(new Date(), 'Москва, улица Туполева, 2'));
+
 // console.log(cart.getSum());
 // console.log(cart.checkOut());
+
 // -- 6 --
 /*
 Необходимо реализовать абстрактный класс Logger с 2-мя методами
@@ -182,19 +213,25 @@ printDate - выводящий в log дату
 К нему необходимо сделать реальный класс, который бы имел метод: logWithDate,
 выводящий сначала дату, а потом заданное сообщение
 */
-class Logger {
-    printDate(date) {
-        this.log(date.toString());
-    }
-}
-class MyLogger extends Logger {
-    log(message) {
-        console.log(message);
-    }
-    logWithDate(message) {
-        this.printDate(new Date());
-        this.log(message);
-    }
-}
-const logger = new MyLogger();
-logger.logWithDate('My message');
+
+// abstract class Logger {
+//   abstract log(message: string):void;
+
+//   printDate(date: Date) {
+//     this.log(date.toString());
+//   }
+// }
+
+// class MyLogger extends Logger {
+//   log(message: string): void {
+//     console.log(message);
+//   }
+//   logWithDate(message: string) {
+//     this.printDate(new Date());
+//     this.log(message);
+
+//   }
+// }
+
+// const logger = new MyLogger();
+// logger.logWithDate('My message');
